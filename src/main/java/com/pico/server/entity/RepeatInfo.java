575ecc1a332136e783dee7a45330f202f774698f
sam971114 <simonana@naver.com>
2024-12-09 19:04:16 +0900

    feat: repeatday 세부 카테고리 삭제

--- src/main/java/com/pico/server/entity/RepeatInfo.java
+++ src/main/java/com/pico/server/entity/RepeatInfo.java
@@ -1,22 +1,14 @@
 package com.pico.server.entity;
 
-import com.pico.server.enums.RepeatDayType;
 import com.pico.server.enums.RepeatType;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
