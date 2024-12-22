bbc9e750b8a7a0fdfa8a7eed89e9ad7747a868cd
sam971114 <simonana@naver.com>
2024-12-23 00:39:34 +0900

    hotfix: 1년치 schedule 로직 수정 구현

--- src/main/java/com/pico/server/repository/ScheduleRepository.java
+++ src/main/java/com/pico/server/repository/ScheduleRepository.java
@@ -16,6 +16,7 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
     @Query("SELECT s FROM Schedule s " +
         "WHERE s.user.id = :userId " +
+        "AND s.isRepeat = false " +
         "AND (" +
         "  (FUNCTION('DATE_FORMAT', s.startTime, '%Y-%m-%d %H:%i') >= FUNCTION('DATE_FORMAT', :startDate, '%Y-%m-%d %H:%i') " +
         "   AND FUNCTION('DATE_FORMAT', s.startTime, '%Y-%m-%d %H:%i') <= FUNCTION('DATE_FORMAT', :endDate, '%Y-%m-%d %H:%i')) " +
