7d6e32b1ac8079478a9d7f5bfa2a10f3a653ffb9
sam971114 <simonana@naver.com>
2025-01-17 14:24:55 +0900

    feat: spring-cloud 의존성 삭제

--- src/main/java/com/pico/server/PicoBeApplication.java
+++ src/main/java/com/pico/server/PicoBeApplication.java
@@ -3,7 +3,6 @@ package com.pico.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
