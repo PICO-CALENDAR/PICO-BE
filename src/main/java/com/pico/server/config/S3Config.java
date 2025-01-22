f992b28305420c2b4daca21d4c27960a12ac21f3
sam971114 <simonana@naver.com>
2025-01-22 21:13:05 +0900

    feat: 이미지 업로드 service code 구현

--- src/main/java/com/pico/server/config/S3Config.java
+++ src/main/java/com/pico/server/config/S3Config.java
@@ -5,8 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
@@ -20,6 +22,16 @@ public class S3Config {
         AwsBasicCredentials credentials = AwsBasicCredentials.create(s3Properties.accessKey(), s3Properties.secretKey());
 
         return S3Presigner.builder()
+            .credentialsProvider(StaticCredentialsProvider.create(credentials))
+            .region(Region.AP_NORTHEAST_2)
+            .build();
+    }
+
+    @Bean
+    public S3Client s3Client() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(s3Properties.accessKey(), s3Properties.secretKey());
+
+        return S3Client.builder()
             .credentialsProvider(StaticCredentialsProvider.create(credentials))
             .region(Region.AP_NORTHEAST_2)
             .build();
--- src/main/java/com/pico/server/service/S3Service.java
+++ src/main/java/com/pico/server/service/S3Service.java
@@ -1,11 +1,21 @@
 package com.pico.server.service;
 
 import com.pico.server.properties.S3Properties;
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
 
 @Service
 @RequiredArgsConstructor
@@ -15,9 +25,29 @@ public class S3Service {
 
     private final S3Presigner preSigner;
     private final S3Properties s3Properties;
-
+    private final S3Client s3Client;
     public String getUserImage(String filename) {
         return getPreSignedUrl(USER_IMAGE, filename);
+    }
+
+    public String putUserImage(MultipartFile uploadFile, String fileName) {
+        return putPreSignedUrl(uploadFile, USER_IMAGE, fileName);
+    }
+
+    public String putUserMultipartImage(MultipartFile uploadFile, String fileName)
+        throws IOException {
+        PutObjectRequest objectRequest =  putObjectRequest(uploadFile, USER_IMAGE, fileName);
+        s3Client.putObject(objectRequest, RequestBody.fromBytes(uploadFile.getBytes()));
+        return s3Client.utilities().getUrl(builder -> builder.bucket(s3Properties.bucket()).key(fileName)).toExternalForm();
+    }
+
+    private PutObjectRequest putObjectRequest(MultipartFile uploadFile, String folder, String filename) {
+        return PutObjectRequest.builder()
+            .bucket(s3Properties.bucket())
+            .key(String.join("/", folder, filename))
+            .contentType(uploadFile.getContentType())
+            .contentLength(uploadFile.getSize())
+            .build();
     }
 
     private String getPreSignedUrl(String folder, String filename) {
@@ -33,6 +63,7 @@ public class S3Service {
         return url;
     }
 
+
     private GetObjectPresignRequest getObjectPresignRequest(String folder, String filename) {
         return GetObjectPresignRequest.builder()
             .signatureDuration(Duration.ofMinutes(1))
@@ -40,6 +71,31 @@ public class S3Service {
                 objectRequest
                     .bucket(s3Properties.bucket())
                     .key(String.join("/", folder, filename)))
+            .build();
+    }
+
+    private String putPreSignedUrl(MultipartFile uploadfile, String folder, String filename) {
+        if (filename == null || filename.isEmpty()) {
+            return null;
+        }
+        String url = preSigner
+            .presignPutObject(putObjectPresignRequest(uploadfile, folder, filename))
+            .url()
+            .toString();
+
+        preSigner.close();
+        return url;
+    }
+
+    private PutObjectPresignRequest putObjectPresignRequest(MultipartFile uploadFile, String folder, String filename) {
+        return PutObjectPresignRequest.builder()
+            .signatureDuration(Duration.ofMinutes(1))
+            .putObjectRequest(objectRequest ->
+                objectRequest
+                    .bucket(s3Properties.bucket())
+                    .key(String.join("/", folder, filename))
+                    .contentType(uploadFile.getContentType())
+                    .contentLength(uploadFile.getSize()))
             .build();
     }
 }
