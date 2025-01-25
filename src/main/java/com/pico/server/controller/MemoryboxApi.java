331a8f3f7ba30dd8a0ecfe809d89c055b54775d9
sam971114 <simonana@naver.com>
2025-01-25 17:30:28 +0900

    feat: swagger 예외 명시

--- src/main/java/com/pico/server/controller/MemoryboxApi.java
+++ src/main/java/com/pico/server/controller/MemoryboxApi.java
@@ -4,11 +4,15 @@ import com.pico.server.dto.request.MemoryboxPartnerRequest;
 import com.pico.server.dto.request.MemoryboxRequest;
 import com.pico.server.dto.request.MemoryboxUpdateRequest;
 import com.pico.server.dto.response.AnniversaryResponse;
+import com.pico.server.dto.response.ErrorResponse;
 import com.pico.server.dto.response.ListResponse;
 import com.pico.server.dto.response.MemoryboxResponse;
 import com.pico.server.security.config.userid.LoginUserId;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -35,6 +39,29 @@ public interface MemoryboxApi {
 
     @PostMapping("/add/{anniversaryId}")
     @Operation(summary = "추억함 추가", description = "기념일에 해당하는 추억함을 생성합니다.")
+    @ApiResponse(responseCode = "404", description = "NOT FOUND", content = @Content(
+        mediaType = "application/json",
+        examples = {
+            @ExampleObject(name = "US0001", description = "사용자를 찾을 수 없는 경우 발생합니다",
+                value = """
+                                    {"code": "US0001", "message": "해당 사용자를 찾을 수 없습니다."}
+                                    """
+            ),
+            @ExampleObject(name = "AN0001", description = "기념일을 찾을 수 없는 경우 발생합니다",
+                value = """
+                                    {"code": "AN0001", "message": "해당 기념일을 찾을 수 없습니다."}
+                                    """
+            )
+        }, schema = @Schema(implementation = ErrorResponse.class)))
+    @ApiResponse(responseCode = "400", description = "BAD REQUEST", content = @Content(
+        mediaType = "application/json",
+        examples = {
+            @ExampleObject(name = "MB0002", description = "같은 일정에 대해 추억함을 중복해 생성하려 할 때 발생합니다.",
+                value = """
+                                    {"code": "MB0002", "message": "같은 일정에 대해서는 추억함을 1개만 생성할 수 있습니다."}
+                                    """
+            )
+        }, schema = @Schema(implementation = ErrorResponse.class)))
     ResponseEntity<MemoryboxResponse> saveMemorybox(
         @Parameter(hidden = true)
         @LoginUserId Long userId,
@@ -45,6 +72,15 @@ public interface MemoryboxApi {
 
     @PostMapping("/add/partner/{memoryboxId}")
     @Operation(summary = "기존 추억함에 상대방 편지 및 사진 추가", description = "이미 연인이 생성한 추억함에 편지와 사진을 추가합니다.")
+    @ApiResponse(responseCode = "404", description = "NOT FOUND", content = @Content(
+        mediaType = "application/json",
+        examples = {
+            @ExampleObject(name = "MB0001", description = "추억함을 찾을 수 없는 경우 발생합니다",
+                value = """
+                                    {"code": "MB0001", "message": "해당 추억함을 찾을 수 없습니다."}
+                                    """
+            )
+        }, schema = @Schema(implementation = ErrorResponse.class)))
     ResponseEntity<MemoryboxResponse> addPartnerMemory(
         @Parameter(hidden = true)
         @LoginUserId Long userId,
@@ -55,6 +91,25 @@ public interface MemoryboxApi {
 
     @PatchMapping("/update/{memoryboxId}")
     @Operation(summary = "추억함 수정", description = "기념일에 해당하는 추억함의 편지와 사진을 수정합니다.")
+    @ApiResponse(responseCode = "404", description = "NOT FOUND", content = @Content(
+        mediaType = "application/json",
+        examples = {
+            @ExampleObject(name = "MB0001", description = "추억함을 찾을 수 없는 경우 발생합니다",
+                value = """
+                                    {"code": "MB0001", "message": "해당 추억함을 찾을 수 없습니다."}
+                                    """
+            ),
+            @ExampleObject(name = "LT0001", description = "편지를 찾을 수 없는 경우 발생합니다",
+                value = """
+                                    {"code": "LT0001", "message": "해당 편지를 찾을 수 없습니다."}
+                                    """
+            ),
+            @ExampleObject(name = "PH0001", description = "사진을 찾을 수 없는 경우 발생합니다",
+                value = """
+                                    {"code": "PH0001", "message": "해당 사진을 찾을 수 없습니다."}
+                                    """
+            )
+        }, schema = @Schema(implementation = ErrorResponse.class)))
     ResponseEntity<MemoryboxResponse> updateMemorybox(
         @Parameter(hidden = true)
         @LoginUserId Long userId,
@@ -67,6 +122,15 @@ public interface MemoryboxApi {
 
     @DeleteMapping("/delete/{memoryboxId}")
     @Operation(summary = "추억함 삭제", description = "기념일에 해당하는 추억함을 삭제합니다.")
+    @ApiResponse(responseCode = "404", description = "NOT FOUND", content = @Content(
+        mediaType = "application/json",
+        examples = {
+            @ExampleObject(name = "MB0001", description = "추억함을 찾을 수 없는 경우 발생합니다",
+                value = """
+                                    {"code": "MB0001", "message": "해당 추억함을 찾을 수 없습니다."}
+                                    """
+            )
+        }, schema = @Schema(implementation = ErrorResponse.class)))
     ResponseEntity<Boolean> deleteMemorybox(
         @Parameter(hidden = true)
         @LoginUserId Long userId,
@@ -75,6 +139,15 @@ public interface MemoryboxApi {
 
     @DeleteMapping("/delete/couple")
     @Operation(summary = "커플 유저에 해당하는 추억함 삭제", description = "내가 생성한 추억함과 상대방이 생성한 추억함 모두를 삭제합니다.")
+    @ApiResponse(responseCode = "404", description = "NOT FOUND", content = @Content(
+        mediaType = "application/json",
+        examples = {
+            @ExampleObject(name = "MB0001", description = "추억함을 찾을 수 없는 경우 발생합니다",
+                value = """
+                                    {"code": "MB0001", "message": "해당 추억함을 찾을 수 없습니다."}
+                                    """
+            )
+        }, schema = @Schema(implementation = ErrorResponse.class)))
     ResponseEntity<Boolean> deleteCoupleMemorybox(
         @Parameter(hidden = true)
         @LoginUserId Long userId
@@ -82,6 +155,15 @@ public interface MemoryboxApi {
 
     @GetMapping("/get/anniversary/{anniveraryId}")
     @Operation(summary = "기념일에 해당하는 추억함 조회", description = "나와 연인이 생성한 해당 기념일의 추억함들을 조회합니다.")
+    @ApiResponse(responseCode = "404", description = "NOT FOUND", content = @Content(
+        mediaType = "application/json",
+        examples = {
+            @ExampleObject(name = "US0001", description = "사용자를 찾을 수 없는 경우 발생합니다",
+                value = """
+                                    {"code": "US0001", "message": "해당 사용자를 찾을 수 없습니다."}
+                                    """
+            )
+        }, schema = @Schema(implementation = ErrorResponse.class)))
     ResponseEntity<ListResponse<MemoryboxResponse>> getAnniversaryMemoryboxes(
         @Parameter(hidden = true)
         @LoginUserId Long userId,
@@ -90,6 +172,15 @@ public interface MemoryboxApi {
 
     @GetMapping("/get/all")
     @Operation(summary = "나와 상대방의 전체 추억함 조회", description = "나와 연인이 생성한 전체 추억함들을 조회합니다.")
+    @ApiResponse(responseCode = "404", description = "NOT FOUND", content = @Content(
+        mediaType = "application/json",
+        examples = {
+            @ExampleObject(name = "US0001", description = "사용자를 찾을 수 없는 경우 발생합니다",
+                value = """
+                                    {"code": "US0001", "message": "해당 사용자를 찾을 수 없습니다."}
+                                    """
+            )
+        }, schema = @Schema(implementation = ErrorResponse.class)))
     ResponseEntity<ListResponse<MemoryboxResponse>> getAllMemoryboxes(
         @Parameter(hidden = true)
         @LoginUserId Long userId
