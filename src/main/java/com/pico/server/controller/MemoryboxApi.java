e8f7ca9ed9f2807193a772b7f198a8d04918ce94
sam971114 <simonana@naver.com>
2025-01-25 17:32:02 +0900

    feat: 추억함 api jwt 관련 속성 추가

--- src/main/java/com/pico/server/controller/MemoryboxApi.java
+++ src/main/java/com/pico/server/controller/MemoryboxApi.java
@@ -14,6 +14,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.io.IOException;
@@ -29,6 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "추억함 API", description = "추억함 관련 API")
 @RestController
+@SecurityRequirement(name = "JWT")
 @RequestMapping("/api/v1/memorybox")
 @ApiResponse(responseCode = "200", description = "OK")
 public interface MemoryboxApi {
