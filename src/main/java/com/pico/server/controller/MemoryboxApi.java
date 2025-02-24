04e9f920789cc294f726b8719e97cc8924b2b68f
sam971114 <simonana@naver.com>
2025-02-24 12:49:18 +0900

    feat: request ModelAttribute로 변경

--- src/main/java/com/pico/server/controller/MemoryboxApi.java
+++ src/main/java/com/pico/server/controller/MemoryboxApi.java
@@ -24,6 +24,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -74,7 +75,7 @@ public interface MemoryboxApi {
         @Parameter(hidden = true)
         @LoginUserId Long userId,
         @Valid
-        @RequestBody MemoryboxRequest request
+        @ModelAttribute MemoryboxRequest request
     ) throws IOException;
 
     @PatchMapping(value = "/update/{memoryboxId}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
